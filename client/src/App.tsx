402377e0f372020fa2daff65ae5533cc6d5cf151
Abhavya28 <abhavya2801@gmail.com>
2026-03-09 23:27:56 +0530

    Navbar added

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import LenisScroll from './components/lenis';
+import Navbar from './components/Navbar';
+import SoftBackdrop from './components/SoftBackdrop';
+
+function App() {
+	return (
+		<>
+			<SoftBackdrop />
+			<LenisScroll />
+			<Navbar />
+		</>
+	);
+}
+export default App;
